b9b544457124cddd577522069251149b9506e5ee
tomxu <cntomxu@gmail.com>
2013-11-15 13:42:10 +0800

    预约画面着手

---
+++ DiandianIOS/DiandianIOS/DAMyTableReservationController.m
@@ -0,0 +1,38 @@
+//
+//  DAMyTableReservationController.m
+//  DiandianIOS
+//
+//  Created by Xu Yang on 11/14/13.
+//  Copyright (c) 2013 DAC. All rights reserved.
+//
+
+#import "DAMyTableReservationController.h"
+
+@interface DAMyTableReservationController ()
+
+@end
+
+@implementation DAMyTableReservationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
